3f9ef2bb1bc631c31e1a05f4288d53e77f4147d9
Matthew Suozzo <matthew.suozzo@gmail.com>
2015-10-02 22:10:49 -0400

    Reversed timeout patch to regain parity with main project

--- gspread/client.py
+++ gspread/client.py
@@ -41,8 +41,6 @@ class Client(object):
                  oauth2client library. https://github.com/google/oauth2client
     :param http_session: (optional) A session object capable of making HTTP requests while persisting headers.
                                     Defaults to :class:`~gspread.httpsession.HTTPSession`.
-    :param request_timeout: (optional) An integral number of seconds to wait before
-                                        a client call will timeout.
 
     >>> c = gspread.Client(auth=('user@example.com', 'qwertypassword'))
 
@@ -52,10 +50,9 @@ class Client(object):
 
 
     """
-    def __init__(self, auth, http_session=None, request_timeout=None):
+    def __init__(self, auth, http_session=None):
         self.auth = auth
         self.session = http_session or HTTPSession()
-        self.request_timeout = request_timeout
 
     def _get_auth_token(self, content):
         for line in content.splitlines():
@@ -97,7 +94,7 @@ class Client(object):
                     (hasattr(self.auth, 'access_token_expired') and self.auth.access_token_expired):
                 import httplib2
 
-                http = httplib2.Http(timeout=self.request_timeout)
+                http = httplib2.Http()
                 self.auth.refresh(http)
 
             self.session.add_header('Authorization', "Bearer " + self.auth.access_token)
@@ -309,43 +306,29 @@ class Client(object):
 
         return ElementTree.fromstring(r.read())
 
-    @property
-    def request_timeout(self):
-        return self.session.timeout
 
-    @request_timeout.setter
-    def request_timeout(self, value):
-        self.session.timeout = value
-
-
-def login(email, password, timeout=None):
+def login(email, password):
     """Login to Google API using `email` and `password`.
 
     This is a shortcut function which instantiates :class:`Client`
     and performs login right away.
 
-    :param timeout: (optional) An integral number of seconds to wait before
-                                any client call (including login) will timeout.
-
     :returns: :class:`Client` instance.
 
     """
-    client = Client(auth=(email, password), request_timeout=timeout)
+    client = Client(auth=(email, password))
     client.login()
     return client
 
-def authorize(credentials, timeout=None):
+def authorize(credentials):
     """Login to Google API using OAuth2 credentials.
 
     This is a shortcut function which instantiates :class:`Client`
     and performs login right away.
 
-    :param timeout: (optional) An integral number of seconds to wait before
-                                any client call (including authorization) will timeout.
-
     :returns: :class:`Client` instance.
 
     """
-    client = Client(auth=credentials, request_timeout=timeout)
+    client = Client(auth=credentials)
     client.login()
     return client
--- gspread/httpsession.py
+++ gspread/httpsession.py
@@ -17,32 +17,25 @@ except ImportError:
     from urllib.parse import urlparse
     from urllib.parse import urlencode
 
-from os import errno
-from ssl import SSLError
-from socket import timeout as SocketTimeout
-
 try:
     unicode
 except NameError:
     basestring = unicode = str
 
 
-from .exceptions import HTTPError, TimeoutError
+from .exceptions import HTTPError
 
 
 class HTTPSession(object):
 
     """Handles HTTP activity while keeping headers persisting across requests.
 
-       :param headers: (optional) A dict with initial headers.
-       :param timeout: (optional) An integral number of seconds to wait for an
-                                    HTTP response before timing out.
+       :param headers: A dict with initial headers.
     """
 
-    def __init__(self, headers=None, timeout=None):
+    def __init__(self, headers=None):
         self.headers = headers or {}
         self.connections = {}
-        self.timeout = timeout
 
     def request(self, method, url, data=None, headers=None):
         if data and not isinstance(data, basestring):
@@ -57,10 +50,13 @@ class HTTPSession(object):
         # If connection for this scheme+location is not established, establish
         # it.
         uri = urlparse(url)
-        addr = uri.scheme + uri.netloc
-        if addr not in self.connections:
-            conn_class = client.HTTPSConnection if uri.scheme == 'https' else client.HTTPException
-            self.connections[addr] = conn_class(uri.netloc, timeout=self.timeout)
+        if not self.connections.get(uri.scheme + uri.netloc):
+            if uri.scheme == 'https':
+                self.connections[
+                    uri.scheme + uri.netloc] = client.HTTPSConnection(uri.netloc)
+            else:
+                self.connections[
+                    uri.scheme + uri.netloc] = client.HTTPConnection(uri.netloc)
 
         request_headers = self.headers.copy()
 
@@ -71,21 +67,9 @@ class HTTPSession(object):
                 else:
                     request_headers[k] = v
 
-        self.connections[addr].request(method, url, data, headers=request_headers)
-        try:
-            response = self.connections[addr].getresponse()
-        except (SocketTimeout, SSLError) as err:
-            # Adapted from the requests package
-            #TODO: Python 3 system EAGAIN handling
-            blocking_errnos = (errno.EAGAIN, errno.EWOULDBLOCK)
-            if any((
-                    isinstance(err, SocketTimeout),        # Python 3
-                    hasattr(err, 'errno') \
-                        and err.errno in blocking_errnos,  # Python 2 EAGAIN
-                    'timed out' in str(err),               # Python 2.7 SSL
-                    'did not complete (read)' in str(err)  # Python 2.7> SSL
-                    )):
-                raise TimeoutError('Request timed out')
+        self.connections[
+            uri.scheme + uri.netloc].request(method, url, data, headers=request_headers)
+        response = self.connections[uri.scheme + uri.netloc].getresponse()
 
         if response.status > 399:
             raise HTTPError(response.status, "%s: %s" % (response.status, response.read()))
